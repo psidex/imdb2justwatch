f9a9434efaf6117f8e123b6d65ae61ebb7c08c80
thatguywiththatname <simjenner3@gmail.com>
2021-01-23 03:21:07 +0000

    Get actual title instead of from tab

--- background.js
+++ background.js
@@ -1,21 +1,18 @@
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    const url = new URL(tab.url);
-    if (url.href.includes('imdb.com/title')) {
+    if (tab.url.includes('imdb.com/title')) {
         browser.pageAction.show(tab.id);
     }
 });
 
-browser.pageAction.onClicked.addListener(() => {
-    browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-        const tab = tabs[0];
-        const movieTitle = tab.title.replace(' - IMDb', '');
-
-        const url = `https://apis.justwatch.com/content/titles/en_GB/popular?language=en&body={"page_size":1,"page":1,"query":"${movieTitle}","content_types":["show","movie"]}`;
-        fetch(encodeURI(url))
-            .then((response) => response.json())
-            .then((data) => {
-                const loc = encodeURI(data.items[0].full_path);
-                browser.tabs.update(tab.id, { url: `https://www.justwatch.com${loc}` });
-            });
+browser.pageAction.onClicked.addListener(async (tab) => {
+    const [movieTitle] = await browser.tabs.executeScript(tab.id, {
+        code: 'document.querySelector(\'.title_wrapper\').querySelector(\'h1\').textContent.trim()',
+    });
+    const url = `https://apis.justwatch.com/content/titles/en_GB/popular?language=en&body={"page_size":1,"page":1,"query":"${movieTitle}","content_types":["show","movie"]}`;
+    const response = await fetch(encodeURI(url));
+    const data = await response.json();
+    const loc = data.items[0].full_path;
+    await browser.tabs.update(tab.id, {
+        url: encodeURI(`https://www.justwatch.com${loc}`),
     });
 });
