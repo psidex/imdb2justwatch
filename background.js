26ea6b2b462a1bb862b67ee0799520cb21713b46
thatguywiththatname <simjenner3@gmail.com>
2021-01-25 01:31:06 +0000

    fix

--- background.js
+++ background.js
@@ -5,14 +5,20 @@ browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 });
 
 browser.pageAction.onClicked.addListener(async (tab) => {
-    const [movieTitle] = await browser.tabs.executeScript(tab.id, {
+    let [movieTitle] = await browser.tabs.executeScript(tab.id, {
         code: 'document.querySelector(\'.title_wrapper\').querySelector(\'h1\').textContent.trim()',
     });
+
+    // NOTE: IMDb uses non-breaking spaces to separate the title and date, so replace all whitespace
+    // with normal spaces.
+    movieTitle = movieTitle.replace(/\s/g, ' ');
+
     const url = `https://apis.justwatch.com/content/titles/en_GB/popular?language=en&body={"page_size":1,"page":1,"query":"${movieTitle}","content_types":["show","movie"]}`;
     const response = await fetch(encodeURI(url));
     const data = await response.json();
+
     const loc = data.items[0].full_path;
     await browser.tabs.update(tab.id, {
-        url: encodeURI(`https://www.justwatch.com${loc}`),
+        url: `https://www.justwatch.com${loc}`,
     });
 });
