8d59195a0c7ce55091ed830424b7600c4540b955
Simon Jenner <simjenner3@gmail.com>
2024-03-01 19:32:34 +0000

    Use new justwatch graphql api

--- background.js
+++ background.js
@@ -15,6 +15,24 @@ const getTitleScript = `
 })();
 `;
 
+async function queryJustWatch(locale, movieTitle) {
+    const response = await fetch('https://apis.justwatch.com/graphql', {
+        credentials: 'omit',
+        headers: {
+            Accept: '*/*',
+            'content-type': 'application/json',
+        },
+        referrer: 'https://www.justwatch.com/',
+        // body taken from justwatch.com search box
+        body: `{"operationName":"GetSuggestedTitles","variables":{"country":"${locale}","language":"en","first":1,"filter":{"searchQuery":"${movieTitle}"}},"query":"query GetSuggestedTitles($country: Country!, $language: Language!, $first: Int!, $filter: TitleFilter) {\\n  popularTitles(country: $country, first: $first, filter: $filter) {\\n    edges {\\n      node {\\n        ...SuggestedTitle\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment SuggestedTitle on MovieOrShow {\\n  id\\n  objectType\\n  objectId\\n  content(country: $country, language: $language) {\\n    fullPath\\n    title\\n    originalReleaseYear\\n    posterUrl\\n    fullPath\\n    __typename\\n  }\\n  watchNowOffer(country: $country, platform: WEB) {\\n    id\\n    standardWebURL\\n    package {\\n      id\\n      packageId\\n      __typename\\n    }\\n    __typename\\n  }\\n  offers(country: $country, platform: WEB) {\\n    monetizationType\\n    presentationType\\n    standardWebURL\\n    package {\\n      id\\n      packageId\\n      __typename\\n    }\\n    id\\n    __typename\\n  }\\n  __typename\\n}\\n"}`,
+        method: 'POST',
+        mode: 'cors',
+    });
+    const obj = await response.json();
+    const loc = obj.data.popularTitles.edges[0].node.content.fullPath;
+    return `https://www.justwatch.com${loc}`;
+}
+
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (tab.url.includes('imdb.com/title')) {
         browser.pageAction.show(tab.id);
@@ -33,18 +51,18 @@ browser.pageAction.onClicked.addListener(async (tab) => {
     let { locale, openInNewTab } = await browser.storage.sync.get(['locale', 'openInNewTab']);
 
     if (locale === undefined || locale === null || locale === '') {
-        locale = 'en_GB';
+        locale = 'GB';
+    }
+    if (locale.includes('_')) {
+        // Convert old-style (en_GB) to new style (GB)
+        [, locale] = locale.split('_');
     }
     if (openInNewTab === undefined || openInNewTab === null || openInNewTab === '') {
         openInNewTab = false;
     }
 
-    const url = `https://apis.justwatch.com/content/titles/${locale}/popular?language=en&body={"page_size":1,"page":1,"query":"${movieTitle}","content_types":["show","movie"]}`;
-    const response = await fetch(encodeURI(url));
-    const data = await response.json();
-
-    const loc = data.items[0].full_path;
-    const opts = { url: `https://www.justwatch.com${loc}` };
+    const url = await queryJustWatch(locale, movieTitle);
+    const opts = { url };
 
     if (openInNewTab === true) {
         await browser.tabs.create(opts);
